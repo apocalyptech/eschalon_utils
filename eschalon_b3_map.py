6bce462b28d21c6044fe5524da695d200a1a5fc6
CJ Kucera <pez@apocalyptech.com>
2014-03-30 22:39:11 -0500

    Remove the map CLI options, which haven't been working properly for ages anyway.  This is related to Issue #41

--- eschalon_b3_map.py
+++ eschalon_b3_map.py
@@ -21,7 +21,7 @@
 
 import getopt, sys
 from eschalon.map import Map
-from eschalon.mapcli import MapCLI
+from eschalon.mapgui import MapGUI
 from eschalon.preferences import Prefs
 
 def usage(full=False):
@@ -31,53 +31,9 @@ def usage(full=False):
     print "To launch the GUI:"
     print "\t%s [<mapfile>]" % (progname)
     print
-    print "To list map attributes on the console:"
-    print "\t%s -l [-s <tiles|objects|txtmap>] [-u] <mapfile>" % (progname)
-    print "\t%s --list [--show=<tiles|...>] [--unknowns] <mapfile>" % (progname)
-    print
     if (full):
-        print "Wherever <mapfile> appears in the above, you should specify the"
-        print "location of the file named with a '.map' extension.  This utility"
-        print "can load both maps that are inside your savegame folder, and the"
-        print "stock maps in Eschalon Book 3's 'data' directory."
-        print
-        print "By default, the application will launch the GUI.  Note that"
-        print "specifying a map file is optional when you're launching"
-        print "the GUI, but required when using any of the other commandline"
-        print "options."
-        print
-        print "For a textual representation of the map instead, use -l or"
-        print "--list.  Note that the commandline options aren't terribly useful,"
-        print "but you'll at least get some information out of them."
-        print
-        print "By default, the --list option will only show the basic map"
-        print "information.  To get a text listing of the attributes of every"
-        print "single tile on the map (you'll probabably want to redirect this"
-        print "to a file, since there are 20,000 tiles in an Eschalon map,"
-        print "though the utility will skip empty tiles), you would use:"
-        print
-        print "\t%s -l -s tiles <mapfile>" % (progname)
-        print "\tor"
-        print "\t%s --list --show=tiles <mapfile>" % (progname)
-        print
-        print "If you wanted a listing of all the objects in the map, specify"
-        print "'objects' instead of 'tiles'."
-        print
-        print "When being shown the listing, specify -u or --unknowns to"
-        print "also show unknown data from the map file."
-        print
-        print "I left in the first visual feature I implemented, which is a text"
-        print "view of the map, which'll simply draw an asterisk where there's"
-        print "a wall, and blank otherwise.  You'll need a really tiny console"
-        print "font and a largeish console window for this to make sense.  On my"
-        print "system's xterm, the 'tiny' font setting seemed about good enough."
-        print "This option's even less useful than the other console options, of"
-        print "course, but I'll leave it in regardless.  To show the text-based"
-        print "map:"
-        print
-        print "\t%s -l -s txtmap <mapfile>" % (progname)
-        print "\tor"
-        print "\t%s --list --show=txtmap <mapfile>" % (progname)
+        print "To load a map automatically without going through the GUI dialog,"
+        print "specify the location of the file named with a '.map' extension."
         print
         print "Additionally, you may use -h or --help to view this message"
     else:
@@ -91,15 +47,6 @@ def main(argv=None):
 
     # Argument var defaults
     options = {
-            'gui': True,
-            'list': False,
-            'listoptions' : {
-                'all': False,
-                'tiles': False,
-                'objects': False,
-                'txtmap': False
-                },
-            'unknowns': False,
             'filename' : None
             }
 
@@ -108,11 +55,8 @@ def main(argv=None):
         argv = sys.argv
     try:
         opts, args = getopt.getopt(argv[1:],
-                'hls:u',
-                ['help',
-                 'list',
-                 'show=',
-                 'unknowns'])
+                'h',
+                ['help'])
     except getopt.GetoptError, err:
         print str(err)
         usage()
@@ -121,20 +65,6 @@ def main(argv=None):
     for o, a in opts:
         if (o in ('-h', '--help')):
             usage(True)
-        elif (o in ('-l', '--list')):
-            options['gui'] = False
-            options['list'] = True
-        elif (o in ('-s', '--show')):
-            if (a == 'tiles'):
-                options['listoptions']['tiles'] = True
-            elif (a == 'objects'):
-                options['listoptions']['objects'] = True
-            elif (a == 'txtmap'):
-                options['listoptions']['txtmap'] = True
-            else:
-                usage()
-        elif (o in ('-u', '--unknowns')):
-            options['unknowns'] = True
         else:
             assert False, 'unhandled option'
 
@@ -144,21 +74,9 @@ def main(argv=None):
         # TODO: this was here to support me doing mass graphic exports.
         # remove it?
         options['filenames'] = args
-
-    # Make sure we have a filename still
-    if (not options['gui'] and options['filename'] == None):
-        print "A filename is required"
-        usage()
     
-    # Now load up the appropriate class
-    if (options['gui']):
-        # We're waiting until now to import, so people just using CLI don't need
-        # PyGTK installed, etc).  Not that this program follows PEP8-recommended
-        # practices anyway, but I *am* aware that doing this is discouraged.
-        from eschalon.mapgui import MapGUI
-        prog = MapGUI(options, Prefs(), 3)
-    else:
-        prog = MapCLI(options, Prefs(), 3)
+    # Launch
+    prog = MapGUI(options, Prefs(), 3)
 
     # ... and run it
     return prog.run()
