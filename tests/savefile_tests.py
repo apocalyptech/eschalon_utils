30863bbb9d2338fde77b5f48acd95a90e2cb7efb
Eitan Adler <lists@eitanadler.com>
2017-09-20 21:52:12 -0700

    Add test for signed int; simplify other tests

--- tests/savefile_tests.py
+++ tests/savefile_tests.py
@@ -54,8 +54,21 @@ class TestSavefile(unittest.TestCase):
         s.open_r()
         self.assertEquals(fn_to_read(s), value_to_expect_on_read)
 
-    def test_write_str(self):
+    def _test_packed_write_and_read(self,
+                                    value,
+                                    pack_string,
+                                    fn_to_write,
+                                    fn_to_read,
+                                    ):
+        self._test_write_and_read(
+            value_to_write=value,
+            raw_value_to_expect_on_read=pack(pack_string, value),
+            value_to_expect_on_read=value,
+            fn_to_write=fn_to_write,
+            fn_to_read=fn_to_read,
+        )
 
+    def test_write_str(self):
         self._test_write_and_read(
             value_to_write="yellow".encode("ascii"),
             raw_value_to_expect_on_read=b"yellow\r\n",
@@ -65,14 +78,28 @@ class TestSavefile(unittest.TestCase):
         )
 
     def test_write_double(self):
-        self._test_write_and_read(
-            value_to_write=1.0,
-            raw_value_to_expect_on_read=pack('d', 1.0),
-            value_to_expect_on_read=1.0,
+        self._test_packed_write_and_read(
+            value=1.4,
+            pack_string='d',
             fn_to_write=eschalon.savefile.Savefile.writedouble,
             fn_to_read=eschalon.savefile.Savefile.readdouble,
         )
 
+    def test_write_int(self):
+        self._test_packed_write_and_read(
+            value=1,
+            pack_string='<I',
+            fn_to_write=eschalon.savefile.Savefile.writeint,
+            fn_to_read=eschalon.savefile.Savefile.readint,
+        )
+
+    def test_write_signed_int(self):
+        self._test_packed_write_and_read(
+            value=-3,
+            pack_string='<i',
+            fn_to_write=eschalon.savefile.Savefile.writesint,
+            fn_to_read=eschalon.savefile.Savefile.readsint,
+        )
 
 if __name__ == '__main__':
     unittest.main()
