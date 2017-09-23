b7696ad362e181f2f71da8abd50545977d890492
Eitan Adler <lists@eitanadler.com>
2017-09-23 14:51:43 -0700

    Try and get Trove classifiers somewhat correct

--- setup.py
+++ setup.py
@@ -29,14 +29,21 @@ setup(
     scripts=[
         'eschalon/main.py',
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': ['eschalon=main:main'],
     },
     test_suite='nose.collector',
-    tests_require= [
+    tests_require=[
         'nose',
         'coverage'
     ],
     include_package_data=True,
-
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: GTK',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Topic :: Games/Entertainment :: Role-Playing',
+        'Topic :: Utilities',
+    ],
 )
