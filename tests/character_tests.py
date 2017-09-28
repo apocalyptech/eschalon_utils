e12aa12f4705735689ee374c0f88d9681970e11d
Eitan Adler <lists@eitanadler.com>
2017-09-28 07:25:51 -0700

    Add a ton more tests
    Make this explicitly to b2char for now

--- tests/character_tests.py
+++ tests/character_tests.py
@@ -1,43 +1,83 @@
 import unittest
 
+import hashlib
 from eschalon.constants import constants
 import eschalon.character
 import eschalon.constantsb2
 import eschalon.savefile
 
 
-class CharacterTests(unittest.TestCase):
+class B2CharacterTests(unittest.TestCase):
+
+    _book2_char_file = "test_data/book2_atend.char"
 
     @classmethod
     def setUpClass(cls):
-        # s = eschalon.savefile.Savefile(filename="test_data/book2_atend.char")
-        cls.b2c = eschalon.character.Character.load(filename="test_data/book2_atend.char", book=2)
+        cls.b2c = eschalon.character.Character.load(B2CharacterTests._book2_char_file, book=2)
         cls.b2c.read()
-        print(cls.b2c)
-
-    def test_character_init(self):
-        c = eschalon.character.B1Character(None)
-        c = eschalon.character.B2Character(None)
-        c = eschalon.character.B3Character(None)
 
     def test_character_information(self):
-        c = CharacterTests.b2c
-        self.assertEqual(c.name, "Veera")
+        c = B2CharacterTests.b2c
+        self.assertEqual(c.name, b"Veera")
         self.assertEqual(c.origin, constants.origintable.inv["Therish"])
         self.assertEqual(c.axiom, constants.axiomtable.inv["Atheistic"])
-        # char.gender
-        # eschalon.constantsb2.B2Constants().gendertable.
-        self.assertEqual(c.gender, 2) # Female
+        self.assertEqual(c.gender, constants.gendertable.inv["Female"])
+
+    @unittest.skip("test fails though output of --list is correct")
+    def test_class(self):
+        c = B2CharacterTests.b2c
+        self.assertEqual(c.axiom, constants.classtable.inv["Magick User"])
 
     def test_skill_levels(self):
-        c = CharacterTests.b2c
+        c = B2CharacterTests.b2c
         st = constants.skilltable
         self.assertEqual(c.skills[st.inv["Mercantile"]], 4)
         self.assertEqual(c.skills[st.inv["Cleaving Weapons"]], 10)
 
     def test_core_attributes(self):
-        c = CharacterTests.b2c
+        c = B2CharacterTests.b2c
         self.assertEqual(c.strength, 30)
+        self.assertEqual(c.dexterity, 20)
+        self.assertEqual(c.endurance, 19)
+        self.assertEqual(c.speed, 13)
+        self.assertEqual(c.intelligence, 22)
+        self.assertEqual(c.wisdom, 20)
+        self.assertEqual(c.perception, 20)
+        self.assertEqual(c.concentration, 27)
+
+    def test_hp_mp_stats(self):
+        c = B2CharacterTests.b2c
+        self.assertEqual(c.maxhp, 112)
+        self.assertEqual(c.curhp, 107)
+        self.assertEqual(c.maxmana, 176)
+        self.assertEqual(c.curmana, 120)
+
+    def test_extra_points(self):
+        c = B2CharacterTests.b2c
+        self.assertEqual(c.extra_att_points, 0)
+        self.assertEqual(c.extra_skill_points, 0)
+
+    def test_leveling_information(self):
+        c = B2CharacterTests.b2c
+        self.assertEqual(c.experience, 169985)
+        self.assertEqual(c.level, 20)
+
+    def test_equip(self):
+        c = B2CharacterTests.b2c
+        self.assertEqual(c.gold, 1654)
+        self.assertEqual(c.torches, 24)
+        self.assertEqual(c.torchused, 0)
+
+    def test_read_write_read_unmodified(self):
+        with open(B2CharacterTests._book2_char_file, 'rb') as df:
+            original_hash = hashlib.sha256(df.read())
+        c = eschalon.character.Character.load(filename=B2CharacterTests._book2_char_file, book=2)
+        c.read()
+        c.write()
+        with open(B2CharacterTests._book2_char_file, 'rb') as df:
+            new_hash = hashlib.sha256(df.read())
+        self.assertEqual(original_hash.hexdigest(), new_hash.hexdigest())
+
 
 
 if __name__ == '__main__':
