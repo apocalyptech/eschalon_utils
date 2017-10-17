1722c7def1722872bce9e3077d51fbec772cffc0
Eitan Adler <lists@eitanadler.com>
2017-10-17 16:36:20 -0700

    Add logger globally to python files

--- eschalon/item.py
+++ eschalon/item.py
@@ -18,6 +18,9 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import logging
+LOG = logging.getLogger(__name__)
+
 from eschalon.constants import constants as c
 
 
