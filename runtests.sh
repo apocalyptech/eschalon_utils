7bee45360f7d0f098d785b5ec51115f8afb16536
Eitan Adler <lists@eitanadler.com>
2017-10-18 04:58:34 -0700

    Make this a real md file rather than pseudo-text

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-mdl README.txt
+mdl .
 python setup.py check
 nosetests-2.7 -c --with-coverage --with-id --detailed-errors "$@"
 nosetests-3.6 -c --with-coverage --with-id --detailed-errors "$@"
