1852e34c9283daf6400bbdf99170122243d1bf38
Eitan Adler <lists@eitanadler.com>
2017-10-22 23:16:36 -0700

    Add more pseudo integration tests

--- scripts/no_unit_test_list.sh
+++ scripts/no_unit_test_list.sh
@@ -2,4 +2,6 @@
 
 # Not everything is unit tested (yet) so help capture errors by running things that should not error. Eventualy add unit tests...
 
+python3 -m eschalon.main --book 1 --list all test_data/book1_atend.char
 python3 -m eschalon.main --book 2 --list all test_data/book2_atend.char
+python3 -m eschalon.main --book 3 --list all test_data/book3_f4_example.char
